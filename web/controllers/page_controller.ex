8c36de8687f199e87236c7acbca3933b28f4b1c2
hayabusa333 <junnichi0121@gmail.com>
2016-10-16 19:40:27 +0900

    csv files handling

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -7,9 +7,15 @@ defmodule Cagliostro.PageController do
     {pwd, 0} = System.cmd("pwd", [])
     csv_dir = String.replace(pwd,"\n","") <> "/csv"
     {:ok, csv_files} = File.ls(csv_dir)
-    {:ok, csv_date} = File.read!(csv_dir <> "/"  <> Enum.at(csv_files, 0) )
-    |> ExCsv.parse
 
-    render conn, "index.html", csv_date: csv_date
+    csv_data = csv_files
+    |> Enum.reduce([], fn(csv_file, acc) ->
+      {:ok, csv} = File.read!(csv_dir <> "/"  <> csv_file )
+      |> ExCsv.parse
+      Logger.info csv_file
+      acc ++ csv
+    end)
+
+    render conn, "index.html", csv_data: csv_data
   end
 end
