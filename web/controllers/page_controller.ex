b66467930722ce390d1d59f36702ce1bb163c4d4
hayabusa333 <junnichi0121@gmail.com>
2016-10-16 17:22:58 +0900

    get csv file date

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,17 @@
 defmodule Cagliostro.PageController do
   use Cagliostro.Web, :controller
 
+  require Logger
+
   def index(conn, _params) do
+    {pwd, 0} = System.cmd("pwd", [])
+    csv_dir = String.replace(pwd,"\n","") <> "/csv"
+    {:ok, csv_files} = File.ls(csv_dir)
+    csv_date = File.read!(csv_dir <> "/"  <> Enum.at(csv_files, 0) )
+
+
+    Logger.info csv_date
+
     render conn, "index.html"
   end
 end
