c6391954197b330f28bb985de970bb0a98511711
hayabusa333 <junnichi0121@gmail.com>
2016-10-16 19:18:29 +0900

    csv date output view

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -7,11 +7,9 @@ defmodule Cagliostro.PageController do
     {pwd, 0} = System.cmd("pwd", [])
     csv_dir = String.replace(pwd,"\n","") <> "/csv"
     {:ok, csv_files} = File.ls(csv_dir)
-    csv_date = File.read!(csv_dir <> "/"  <> Enum.at(csv_files, 0) )
+    {:ok, csv_date} = File.read!(csv_dir <> "/"  <> Enum.at(csv_files, 0) )
+    |> ExCsv.parse
 
-
-    Logger.info csv_date
-
-    render conn, "index.html"
+    render conn, "index.html", csv_date: csv_date
   end
 end
